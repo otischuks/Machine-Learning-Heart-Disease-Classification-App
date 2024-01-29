aedfc6034f7f4b3457109540245c9ca599bf3f25
Onyi Chuks <110092940+onyichuks@users.noreply.github.com>
2024-01-29 12:13:43 -0700

    Add files via upload

---
+++ Heart_disease_app.py
@@ -0,0 +1,195 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+# Import data file, create features and targets
+df = pd.read_csv("C:\Mine\Trainings\Machine Learning &bData Science\ZTM\ML Data science\Python\Projects\Heart Disease\heart-disease.csv")
+y = df['target']
+X = df.drop(['target'], axis = 1)
+
+# Split data into training and test sets
+from sklearn.model_selection import train_test_split
+np.random.seed(42)
+X_train, X_test, y_train,y_test = train_test_split(X,y, test_size = 0.2)
+
+# Train model using best hyperparameters
+from sklearn.linear_model import LogisticRegression
+lr_model = LogisticRegression(C=0.20433597178569418,
+                       solver="liblinear")
+lr_model.fit(X_train,y_train)
+lr_model.score(X_test, y_test)
+Final_Model = lr_model
+
+
+# Build GUI
+import re
+from tkinter import *
+import tkinter as tk
+
+
+# Define function to check inputs
+def check_inputs():
+    if age.get() == "":
+        print("Age Field is Empty!!")
+        Label(win,text="Age Field is Empty!!",fg="blue",bg="yellow",font = ("Calibri 10 bold")).place(x=12,y=580)
+
+
+    elif rbp.get() == "":
+        print("Resting Blood Pressure Field is Empty!!")
+        Label(win,text="Resting Blood Pressure Field is Empty!!",fg="blue",bg="yellow",font = ("Calibri 10 bold")).place(x=12,y=580)
+
+    elif chol.get() == "":
+        print("Cholestrol Field is Empty!!")
+        Label(win,text="Cholestrol Field is Empty!!",fg="blue",bg="yellow",font = ("Calibri 10 bold")).place(x=12,y=580)
+
+    elif heart_rate.get() == "":
+        print("Heart Rate Field is Empty!!")
+        Label(win,text="Heart Rate Field is Empty!!",fg="blue",bg="yellow",font = ("Calibri 10 bold")).place(x=12,y=580)
+    elif peak.get() == "":
+        print("Depression By Exercise Field is Empty!!")
+        Label(win,text="Depression By Exercise Field is Empty!!",fg="blue",bg="yellow",font = ("Calibri 10 bold")).place(x=12,y=580)
+
+    else:
+        predict()
+
+# Define prediction funtion
+def predict():
+    gender_dict = {"Male":1, "Female":0}
+    fbs_dict = {"True":1, "False":0}
+    eia_dict = {"True":1, "False":0}
+    cp_dict = {"0: typical angina":0,"1: atypical angina":1,"2: non-anginal pain":2,"3: asymptomatic":3}
+    thal_dict = {"0: No Test":0,"1: Fixed Defect":1,"2: Normal Flow":2,"3: Reversible Defect":3}
+    Pred_dict = {0:"Prediction: No Heart Disease Detected", 1:"Prediction: Heart Disease Deteced\nYou should consult with your Doctor!"}
+    
+    data = [float(age.get()),gender_dict[str(radio.get())], cp_dict[str(variable.get())], float(rbp.get()),
+           float(chol.get()),fbs_dict[str(radio_fbs.get())], int(str(variable_ecg.get())), float(heart_rate.get()),
+           eia_dict[str(radio_eia.get())], float(peak.get()), int(str(variable_slope.get())),int(str(variable_n_vessels.get())),
+           thal_dict[str(variable_thal.get())]]
+
+    prediction = Final_Model.predict(np.array(data).reshape(1,13))
+    pred_label = Pred_dict[prediction.tolist()[0]]
+    Label(win,text=pred_label,fg="blue",bg="yellow",font = ("Calibri 10 bold")).place(x=12,y=580)
+
+
+# Define reset funtion
+def reset():
+    age.set("")
+    rbp.set("")
+    chol.set("")
+    heart_rate.set("")
+    peak.set("")
+
+
+
+# Define GUI properties and widgets properties
+win =  Tk()
+
+win.geometry("450x600")
+win.configure(background="#Eaedee")
+win.title("Heart Disease Classifier")
+
+
+title = Label(win, text="Heart Disease Classifier", bg="#2583be", width="300", height="2", fg="white", font = ("Arial 20 italic")).pack()
+
+age = Label(win, text="Age in Years", bg="#Eaedee", font=("Verdana 12")).place(x=12, y=65)
+
+rbp = Label(win, text="Resting Blood Pressure ", bg="#Eaedee", font=("Verdana 12")).place(x=12, y=105)
+
+chol = Label(win, text="Cholestrol mg/dl ", bg="#Eaedee", font=("Verdana 12")).place(x=12, y=145)
+
+heart_rate = Label(win, text="Maximum Heart Rate ", bg="#Eaedee", font=("Verdana 12")).place(x=12, y=185)
+
+peak = Label(win, text="Depression By Exercise ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=225)
+
+
+  
+Gender = Label(win, text="Gender ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=265)
+
+radio = StringVar()
+Male = Radiobutton(win, text="Male",bg="#Eaedee",variable=radio,value="Male",font = ("Verdana 12")).place(x=165,y=265)
+Female = Radiobutton(win, text="Female",bg="#Eaedee",variable=radio,value="Female",font = ("Verdana 12")).place(x=265,y=265)
+
+FBS = Label(win, text="Fasting Blood Sugar ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=285)
+
+radio_fbs = StringVar()
+Male = Radiobutton(win, text="True",bg="#Eaedee",variable=radio_fbs,value="True",font = ("Verdana 12")).place(x=165,y=285)
+Female = Radiobutton(win, text="False",bg="#Eaedee",variable=radio_fbs,value="False",font = ("Verdana 12")).place(x=265,y=285)
+
+EIA = Label(win, text="Exercise Induced Angina",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=305)
+
+radio_eia = StringVar()
+Male = Radiobutton(win, text="True",bg="#Eaedee",variable=radio_eia,value="True",font = ("Verdana 12")).place(x=165,y=305)
+Female = Radiobutton(win, text="False",bg="#Eaedee",variable=radio_eia,value="False",font = ("Verdana 12")).place(x=265,y=305)
+
+
+cp = Label(win,text="Chest Pain ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=345)
+variable = StringVar(win)
+variable.set("CP")
+w = OptionMenu(win, variable, "0: typical angina","1: atypical angina","2: non-anginal pain","3: asymptomatic")
+w.place(x=145,y=345)
+
+ecg = Label(win,text="Resting ECG ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=385)
+variable_ecg = StringVar(win)
+variable_ecg.set("ECG")
+w_ecg = OptionMenu(win, variable_ecg, "0","1","2")
+w_ecg.place(x=145,y=385)
+
+
+exer_slope = Label(win,text="Slope of Exercise ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=425)
+variable_slope = StringVar(win)
+variable_slope.set("Slope")
+w_slope = OptionMenu(win, variable_slope, "0","1","2")
+w_slope.place(x=145,y=425)
+
+
+thal_label = Label(win,text="Thallium Stress ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=465)
+variable_thal = StringVar(win)
+variable_thal.set("Thal")
+w_thal = OptionMenu(win, variable_thal, "0: No Test","1: Fixed Defect","2: Normal Flow","3: Reversible Defect")
+w_thal.place(x=145,y=465)
+
+
+n_vessels = Label(win,text="Number Vessels Colored ",bg="#Eaedee",font = ("Verdana 12")).place(x=12,y=505)
+variable_n_vessels = StringVar(win)
+variable_n_vessels.set("N_Vessels")
+w_n_vessels = OptionMenu(win, variable_n_vessels, "0","1","2","3")
+w_n_vessels.place(x=140,y=505)
+
+
+age = StringVar()
+rbp = StringVar()
+chol = StringVar()
+heart_rate = StringVar()
+peak  = StringVar()
+Gender = StringVar()
+FBS  = StringVar()
+EIA  = StringVar()
+cp  = StringVar()
+ecg  = StringVar()
+exer_slope  = StringVar()
+thal_label  = StringVar()
+n_vessels  = StringVar()
+
+entry_age = Entry(win,textvariable = age,width=30)
+entry_age.place(x=150,y=65)
+
+entry_rbp = Entry(win,textvariable = rbp,width=30)
+entry_rbp.place(x=150,y=105)
+
+entry_chol = Entry(win,textvariable = chol,width=30)
+entry_chol.place(x=150,y=145)
+
+entry_heart_rate = Entry(win, textvariable = heart_rate,width=30)
+entry_heart_rate.place(x=150,y=185)
+
+entry_peak = Entry(win,textvariable = peak,width=30)
+entry_peak.place(x=150,y=225)
+
+reset = Button(win, text="Reset", width="12",height="1",activebackground="red",command=reset, bg="Pink",font = ("Calibri 12 ")).place(x=24, y=540)
+submit = Button(win, text="Classify", width="12",height="1",activebackground="violet", bg="Pink",command=check_inputs,font = ("Calibri 12 ")).place(x=240, y=540)
+
+
+win.mainloop()
+
+# Package model into standalone app by running code below via terminal
+# pyinstaller --onefile --windowed --debug=all Heart_disease_app.py
